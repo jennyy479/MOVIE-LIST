10aeb0fa1f1764fb68ba34f56c2756e19c23eea8
Yen <z83825@yahoo.com.tw>
2020-11-05 22:13:58 +0800

    write your meaaage

---
+++ index.js
@@ -0,0 +1,123 @@
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
+const movies = []
+let filteredMovies = [] //搜尋清單
+
+const MOVIES_PER_PAGE = 12
+const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input')
+const paginator = document.querySelector('#paginator')
+
+function renderMovieList(data) {
+  let rawHTML = ''
+  data.forEach((item) => {
+    // title, image
+    rawHTML += `<div class="col-sm-3">
+    <div class="mb-2">
+      <div class="card">
+        <img src="${POSTER_URL + item.image
+      }" class="card-img-top" alt="Movie Poster">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#movie-modal" data-id="${item.id
+      }">More</button>
+          <button class="btn btn-info btn-add-favorite" data-id="${item.id
+      }">+</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+  })
+  dataPanel.innerHTML = rawHTML
+}
+
+function renderPaginator(amount) {
+  const numberOfPages = Math.ceil(amount / MOVIES_PER_PAGE)
+  let rawHTML = ''
+
+  for (let page = 1; page <= numberOfPages; page++) {
+    rawHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`
+  }
+  paginator.innerHTML = rawHTML
+}
+
+function getMoviesByPage(page) {
+  const startIndex = (page - 1) * MOVIES_PER_PAGE
+  return movies.slice(startIndex, startIndex + MOVIES_PER_PAGE)
+}
+
+function showMovieModal(id) {
+  const movieTitle = document.querySelector('#movie-modal-title')
+  const movieImage = document.querySelector('#movie-modal-image')
+  const movieDate = document.querySelector('#movie-modal-date')
+  const movieDescription = document.querySelector('#movie-modal-description')
+
+  axios.get(INDEX_URL + id).then((response) => {
+    const data = response.data.results
+    movieTitle.innerText = data.title
+    movieDate.innerText = 'Release Date: ' + data.release_date
+    movieDescription.innerText = data.description
+    movieImage.innerHTML = `<img src="${POSTER_URL + data.image}" alt="movie-poster">`
+  })
+}
+
+function addToFavorite(id) {
+
+  const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+  const movie = movies.find((movie) => movie.id === id)
+
+  if (list.some((movie) => movie.id === id)) {
+    return alert('此電影已在收藏清單中！')
+  }
+  list.push(movie)
+  localStorage.setItem('favoriteMovies', JSON.stringify(list))
+}
+
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    showMovieModal(Number(event.target.dataset.id))
+  } else if (event.target.matches('.btn-add-favorite')) {
+    addToFavorite(Number(event.target.dataset.id))
+  }
+})
+
+searchForm.addEventListener('submit', function onSearchFormSubmitted(event) {
+
+  event.preventDefault()
+  const keyword = searchInput.value.trim().toLowerCase()
+
+  let filteredMovies = [] //搜尋清單
+
+  // if (!keyword.length) {
+  //   return alert('請輸入有效字串！')
+  // }
+
+  filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(keyword)
+  )
+
+  if (filteredMovies.length === 0) {
+    return alert(`您輸入的關鍵字：${keyword} 沒有符合條件的電影`)
+  }
+  renderMovieList(filteredMovies)
+})
+
+paginator.addEventListener('click', function onPaginatorClicked(event) {
+  if (event.target.tagName !== 'A') return
+
+  const page = Number(event.target.dataset.page)
+  renderMovieList(getMoviesByPage(page))
+})
+
+axios
+  .get(INDEX_URL)
+  .then((response) => {
+    movies.push(...response.data.results)
+    renderPaginator(movies.length)
+    renderMovieList(getMoviesByPage(1))
+  })
+  .catch((err) => console.log(err)) 
